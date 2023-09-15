4adb89361e88d937c069153bd02759e249920f2b
gyeongeunpark <142005747+gyeongeunpark@users.noreply.github.com>
2023-09-15 16:00:37 +0900

    Initial commit

---
+++ react_web_back/src/main/java/kr/or/iei/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package kr.or.iei.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//페이지 이동안하고 데이터만 보내주는 역할 (비동기 컨트롤러)
+@RequestMapping(value="/member")
+public class MemberController {
+
+}
